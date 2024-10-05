5d78683464cb37ce08178b839564532db017542d
DelinQu <dlqu22@m.fudan.edu.cn>
2024-10-05 20:40:17 +0800

    set neotree follow_current_file, and map to <CR>

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -58,7 +58,8 @@ map("n", "a", "ggVG", { desc = "Select all" })
 -- nvimtree -> neo-tree
 map("n", "<C-n>", "<cmd>Neotree close<CR>", { desc = "neotree close window" })
 map("n", "<leader>e", "<cmd>Neotree focus<CR>", { desc = "neotree focus window" })
--- vim.keymap.del('n', '<CR>')
+-- map("n", "<CR>", "<cmd>Neotree filesystem reveal left<CR>", { desc = "neotree reveal left" })
+map("n", "<CR>", "<cmd>Neotree reveal<CR>", { desc = "neotree reveal left" })
 
 -- right vim-tree
 map("n", "<C-m>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
